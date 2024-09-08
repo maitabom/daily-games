68d206b4f1c227a6c982532ab3b98d150c34be46
Fábio Valentim <maitabom@gmail.com>
2024-09-08 17:20:26 -0300

    Criação de tela de perfil

---
+++ src/app/profile/components/favorites/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import { FiEdit, FiX } from "react-icons/fi";
+
+export default function FavoriteCard() {
+  const [input, setInput] = useState("");
+  const [showInput, setShowInput] = useState(false);
+  const [gameName, setGameName] = useState("");
+
+  function handleButton() {
+    setShowInput(!showInput);
+
+    if (input !== "") {
+      setGameName(input);
+    }
+
+    setInput("");
+  }
+
+  return (
+    <div className="w-full bg-gray-900 p-4 h-44 text-white rounded-lg flex justify-between flex-col">
+      {showInput ? (
+        <div className="flex items-center justify-center gap-3">
+          <input
+            className="w-full rounded-md h-8 text-black px-2"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={handleButton}>
+            <FiX size={24} />
+          </button>
+        </div>
+      ) : (
+        <button
+          className="self-start hover:scale-110 duration-200 transition-all"
+          onClick={handleButton}
+        >
+          <FiEdit size={24} />
+        </button>
+      )}
+
+      {gameName && (
+        <div>
+          <span>Jogo Favorito:</span>
+          <p className="font-bold">{gameName}</p>
+        </div>
+      )}
+
+      {!gameName && <p className="font-bold">Adicionar jogo</p>}
+    </div>
+  );
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,9 @@
-import Container from "@/components/container";
 import Image from "next/image";
 
+import Container from "@/components/container";
 import userImg from "@/assets/user.png";
+import FavoriteCard from "./components/favorites";
+
 import { FaShareAlt } from "react-icons/fa";
 
 export default function Profile() {
@@ -18,10 +20,23 @@ export default function Profile() {
             <h1 className="font-bold text-2xl">Usuário do Sistema</h1>
           </div>
           <div className="sm:absolute top-0 right-0 gap-3 flex items-center justify-center mt-2">
-            <button className="bg-gray-700 px-4 py-3 rounded-lg text-white">Configurações</button>
+            <button className="bg-gray-700 px-4 py-3 rounded-lg text-white">
+              Configurações
+            </button>
             <button className="bg-gray-700 px-4 py-3 rounded-lg">
               <FaShareAlt size={24} color="#FFF" />
             </button>
+          </div>
+        </section>
+        <section className="flex flex-wrap gap-5 flex-col md:flex-row">
+          <div className="flex-grow flex-wrap">
+            <FavoriteCard />
+          </div>
+          <div className="flex-grow flex-wrap">
+            <FavoriteCard />
+          </div>
+          <div className="flex-grow flex-wrap">
+            <FavoriteCard />
           </div>
         </section>
       </Container>
